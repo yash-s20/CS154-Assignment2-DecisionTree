5174f52e74a78363fbc1562d14a9599d88bac56f
Yash Sharma <yash.sharma200999@gmail.com>
2019-03-05 16:30:17 +0530

    changes to compilable code

--- src/decision_functions.rkt
+++ src/decision_functions.rkt
@@ -6,10 +6,10 @@
 (provide y3)
 (provide y4>62)
 
-(define (y1 record) (cons "feature1" (...))) ; returns the value of feature 1 for a given test sample
-(define (y2 record) (cons "feature2" (...)))
-(define (y3 record) (cons "feature3" (...)))
-(define (y4>62 record) (cons "feature4>62" (...))) ; returns 1 if the value of feature 4 > 62, else 0
+(define (y1 record) (cons "feature1" '...)) ; returns the value of feature 1 for a given test sample
+(define (y2 record) (cons "feature2" '...))
+(define (y3 record) (cons "feature3" '...))
+(define (y4>62 record) (cons "feature4>62" '...)) ; returns 1 if the value of feature 4 > 62, else 0
 
 ;candidate functions for the titanic dataset
 (provide pclass)
@@ -20,13 +20,13 @@
 (provide fare>50)
 (provide emb)
 
-(define (pclass record) (cons "pclass" (...))) ; returns the value of pclass for a given test sample
-(define (sex record) (cons "sex" (...)))
-(define (age>25 record) (cons "age>25" (...)))
-(define (sibsp record) (cons "sibsp" (...)))
-(define (parch record) (cons "parch" (...)))
-(define (fare>50 record) (cons "fare>50" (...)))
-(define (emb record) (cons "emb" (...)))
+(define (pclass record) (cons "pclass" '...)) ; returns the value of pclass for a given test sample
+(define (sex record) (cons "sex" '...))
+(define (age>25 record) (cons "age>25" '...))
+(define (sibsp record) (cons "sibsp" '...))
+(define (parch record) (cons "parch" '...))
+(define (fare>50 record) (cons "fare>50" '...))
+(define (emb record) (cons "emb" '...))
 
 ;candidate functions for the mushroom dataset
 (provide cshape)
@@ -41,14 +41,14 @@
 (provide pop)
 (provide hab)
 
-(define (cshape record) (cons "cshape" (...)))
-(define (csurf record) (cons "csurf" (...)))
-(define (bruise record) (cons "bruise" (...)))
-(define (odor record) (cons "odor" (...)))
-(define (gatch record) (cons "gatch" (...)))
-(define (gspace record) (cons "gspace" (...)))
-(define (gsize record) (cons "gsize" (...)))
-(define (sshape record) (cons "sshape" (...)))
-(define (nring record) (cons "nring" (...)))
-(define (pop record) (cons "pop" (...)))
-(define (hab record) (cons "hab" (...)))
+(define (cshape record) (cons "cshape" '...))
+(define (csurf record) (cons "csurf" '...))
+(define (bruise record) (cons "bruise" '...))
+(define (odor record) (cons "odor" '...))
+(define (gatch record) (cons "gatch" '...))
+(define (gspace record) (cons "gspace" '...))
+(define (gsize record) (cons "gsize" '...))
+(define (sshape record) (cons "sshape" '...))
+(define (nring record) (cons "nring" '...))
+(define (pop record) (cons "pop" '...))
+(define (hab record) (cons "hab" '...))
--- src/master.rkt
+++ src/master.rkt
@@ -1,16 +1,20 @@
 #lang racket
-
+(require 2htdp/batch-io)
 (require "decision_functions.rkt")
 
 ;input dataset
 (provide toytrain)
 (define toytrain "../data/toy_train.csv")
 
+(define (read-file file-path)
+  (call-with-input-file file-path
+    (lambda (fin) (port->string fin))))
+
 (provide titanictrain)
-...
+(define titanictrain "../data/titanic_train.csv")
 
 (provide mushroomtrain)
-...
+(define mushroomtrain "../data/mushrooms_train.csv")
 
 ;output tree (dot file)
 (provide toyout)
@@ -22,28 +26,28 @@
 ;further split each line at commas
 ;so then we have a list of list of strings
 (provide toy-raw)
-(define toy-raw (...))
+(define toy-raw '...)
 
 (provide titanic-raw)
-...
+(define titanic-raw "../data/titanic_raw.csv")
 
 (provide mushroom-raw)
-...
+(define mushroom-raw "../data/mushroom_raw.csv")
 
 ;function to convert data to internal numerical format
 ;(features . result)
 (provide format)
-(define (format data) (...))
+(define (format data) '...)
 
 ;list of (features . result)
 (provide toy)
-(define toy (...))
+(define toy '...)
 
 (provide titanic)
-...
+(define titanic '...)
 
 (provide mushroom)
-...
+(define mushroom '...)
 
 ;============================================================================================================
 ;============================================================================================================
@@ -53,26 +57,26 @@
 ;used to find probability value at leaf
 (provide get-leaf-prob)
 (define (get-leaf-prob data)
-  ...
+  '...
   )
 
 ;get entropy of dataset
 (provide get-entropy)
 (define (get-entropy data)
-  ...
+  '...
   )
 
 ;find the difference in entropy achieved
 ;by applying a decision function f to the data
 (provide entropy-diff)
 (define (entropy-diff f data)
-  ...
+  '...
   )
 
 ;choose the decision function that most reduces entropy of the data
 (provide choose-f)
 (define (choose-f candidates data) ; returns a decision function
-  ...
+  '...
   )
 
 (provide DTree)
@@ -81,14 +85,14 @@
 ;build a decision tree (depth limited) from the candidate decision functions and data
 (provide build-tree)
 (define (build-tree candidates data depth)
-  ...
+  '...
   )
 
 ;given a test data (features only), make a decision according to a decision tree
 ;returns probability of the test data being classified as 1
 (provide make-decision)
 (define (make-decision tree test)
-  ...
+ '...
   )
 
 ;============================================================================================================
@@ -110,13 +114,13 @@
   (let* ([node (match tree [(DTree d f c) (cons d c)])]
          [f (car node)]
          [c (cdr node)])
-    (string-append tabs "r" prefix "[label=\"" d "\"];" "\n\n" (dot-child (pair-idx c 0) prefix tabs))
+    (string-append tabs "r" prefix "[label=\"" f "\"];" "\n\n" (dot-child (pair-idx c 0) prefix tabs))
     )
   )
 
 ;output tree (dot file)
 (provide display-tree)
-(define (display-tree tree)
+(define (display-tree tree dtfile)
   (write-file dtfile (string-append "graph \"decision-tree\" {" "\n" (dot-helper tree "" "\t") "}"))
   )
 ;============================================================================================================
